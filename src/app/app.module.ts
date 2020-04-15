0055f93d660474e453028fcc0443f746ab5eb8a8
Igor Sushko <igor_sushko@yahoo.com>
2020-04-15 13:45:33 +0500

    Установка иконок
    ng add @angular/material
    
    https://medium.com/techiediaries-com/angular-9-material-by-example-build-an-ui-with-mattoolbar-maticon-matcard-and-matbutton-df1f18a6f800

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
 import { AppComponent } from './app.component';
 import { CarFlashViewComponent } from './car-flash-view/car-flash-view.component';
 import { PanelWithCarsComponent } from './panel-with-cars/panel-with-cars.component';
@@ -10,6 +15,9 @@ import { CarTechSpecComponent } from './car-tech-spec/car-tech-spec.component';
 
 import { PageCreditComponent } from './page-credit/page-credit.component';
 import { CreditMenuComponent } from './credit-menu/credit-menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
@@ -22,7 +30,8 @@ import { CreditMenuComponent } from './credit-menu/credit-menu.component';
     CreditMenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
